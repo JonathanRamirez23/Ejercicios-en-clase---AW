189123fcfc75f72502049a7b4dd979ea59bca726
JonathanRamirez23 <151898463+JonathanRamirez23@users.noreply.github.com>
2024-10-21 15:44:33 -0500

    notas
    Clase N° 5

---
+++ API RESTFUL-GR1/notes.js
@@ -0,0 +1,16 @@
+
+// RUTA: Es el recurso que el servidor debe porcesar a traves de un endpoint
+// REQUEST: La solicitud que el servidor debe procesar 
+    // 1. body : ruta, header, body (se realiza un registro)
+    // 2. params : 
+// RESPONSE: El tipo (5 formas) de recurso que el servidor va a responder 
+
+//MIDDLEWARE:  es un intermediario entre el request y response
+
+
+//FUNDAMENTOS AGNOSTICOS : ETAPA 1
+
+
+
+
+
