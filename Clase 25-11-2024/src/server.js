13e1fd875e6ce58c5bc73fd861339de8f9699aed
JonathanRamirez23 <151898463+JonathanRamirez23@users.noreply.github.com>
2024-11-25 15:49:03 -0500

    Ejercicio en clase
    Clase necesaria para la realizacion de la prueba

---
+++ Clase 25-11-2024/src/server.js
@@ -0,0 +1,58 @@
+// Requerir módulos 
+import express from 'express';
+
+import cloudinary from 'cloudinary';
+import fileUpload from 'express-fileupload';
+import dotenv from 'dotenv';
+
+import routerTour from './routers/tour_routes.js';
+import routerUser from './routers/user_routes.js';
+
+
+dotenv.config()
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+
+// Inicializaciones
+const app = express()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_APY_KEY,
+    api_secret: process.env.CLOUDINARY_APY_SECRET
+})
+
+
+//Se crea una carpeta de manera temporal - express-fileupload
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : './uploads'
+}));
+
+
+
+// Variables 
+app.set('port', process.env.puertito || 3000)
+
+
+
+// Middlewares
+app.use(express.json())
+
+
+// Rutas 
+app.get('/',(req,res)=>{
+    res.send("Server on")
+})
+
+// Rutas - Tour
+app.use('/api',routerTour)
+
+// Rutas - Users
+app.use('/api',routerUser)
+
+
+// Exportar la variable app 
+export default app
+
+
+
